03a10dc65b8ed35819e8ce1dbd5baeb5c2e48ff4
chaabounizeineb123456789 <41632663+chaabounizeineb123456789@users.noreply.github.com>
2018-07-31 10:55:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function italic(){
+    document.getElementById("demo").style.fontStyle="italic";
+}
+function bold(){
+	document.getElementById("demo").style.fontWeight="900";
+}
+function underline(){
+	document.getElementById("demo").style.textDecoration="underline";
+}
+function arial(){
+	document.getElementById("demo").style.fontFamily=document.getElementById("s").value;
+}
+function kabber(){
+	document.getElementById("demo").style.fontSize=document.getElementById("s2").value;
+}
+function loun(){
+	document.getElementById("demo").style.backgroundColor=document.getElementById("s3").value;
+}
+
+function badel(){
+	document.getElementById("track").value;
+}
+
+function myFunction() {
+  document.getElementById("fd").innerHTML = "thank you for your feedback, dear friend !";
+}
+/*function myFunction() {
+    document.getElementById("fd").style.display = "block";
+}*/
